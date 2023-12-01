4a2263805e045257d10d8c77b8cee7532996325c
Logan Lucas <logan.lucas@etu.univ-cotedazur.fr>
2023-12-01 16:38:11 +0100

    feat: add real game districts (#3)

--- src/main/java/com/github/the10xdevs/citadels/models/District.java
+++ src/main/java/com/github/the10xdevs/citadels/models/District.java
@@ -23,7 +23,33 @@ public final class District {
      * List of all the districts in the game
      */
     public static final Set<District> all = Set.of(
-        new District("Baraque de Logan", Category.MERVEILLE, 5)
+        new District("Bibliothèque", Category.MERVEILLE, 6), // x1
+        new District("Caserne", Category.MILITAIRE, 3), // x3
+        new District("Cathédrale", Category.RELIGIEUX, 5), // x2
+        new District("Château", Category.NOBLE, 4), // x4
+        new District("Cimetière", Category.MERVEILLE, 5), // x1
+        new District("Comptoir", Category.MARCHAND, 3), // x3
+        new District("Cour des miracles", Category.MERVEILLE, 2), // x1
+        new District("Donjon", Category.MERVEILLE, 3), // x2
+        new District("Dracoport", Category.MERVEILLE, 8), // x1
+        new District("Echoppe", Category.MARCHAND, 2), // x3
+        new District("Ecole de magie", Category.MERVEILLE, 6), // x1
+        new District("Eglise", Category.RELIGIEUX, 2), // x4
+        new District("Forge", Category.MERVEILLE, 5), // x1
+        new District("Forteresse", Category.MILITAIRE, 5), // x2
+        new District("Hotel de ville", Category.MARCHAND, 5), // x2
+        new District("Laboratoire", Category.MERVEILLE, 5), // x1
+        new District("Manoir", Category.NOBLE, 3), // x5
+        new District("Marché", Category.MARCHAND, 2), // x4
+        new District("Monastère", Category.RELIGIEUX, 3), // x3
+        new District("Observatoire", Category.MERVEILLE, 5), // x1
+        new District("Palais", Category.NOBLE, 5), // x2
+        new District("Port", Category.MARCHAND, 4), // x3
+        new District("Prison", Category.MILITAIRE, 2), // x3
+        new District("Taverne", Category.MARCHAND, 1), // x5
+        new District("Temple", Category.RELIGIEUX, 1), // x3
+        new District("Tour de guet", Category.MILITAIRE, 1), // x3
+        new District("Université", Category.MERVEILLE, 6) // x1
     );
 
     @Override
