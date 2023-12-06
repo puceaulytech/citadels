23092c1e356e26cd3607eaf8497735f07c35a54e
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2023-12-06 12:10:07 +0100

    test: cleanup dummy behavior tests

--- src/test/java/com/github/the10xdevs/citadels/behaviors/DummyBehaviorTest.java
+++ src/test/java/com/github/the10xdevs/citadels/behaviors/DummyBehaviorTest.java
@@ -1,47 +1,22 @@
 package com.github.the10xdevs.citadels.behaviors;
 
-import com.github.the10xdevs.citadels.exceptions.IllegalActionException;
-import com.github.the10xdevs.citadels.gamestate.Player;
-import com.github.the10xdevs.citadels.interaction.actions.RegularTurnAction;
 import com.github.the10xdevs.citadels.interaction.actions.RoleTurnAction;
-import com.github.the10xdevs.citadels.interaction.behaviors.Behavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.DummyBehavior;
-import com.github.the10xdevs.citadels.interaction.views.GameView;
-import com.github.the10xdevs.citadels.interaction.views.SelfPlayerView;
 import com.github.the10xdevs.citadels.models.Role;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
+import java.util.EnumSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class DummyBehaviorTest {
-    @BeforeEach
-    void setUp() {
-        SelfPlayerView self =new SelfPlayerView(new Player(new Behavior() {
-            @Override
-            public void pickRole(RoleTurnAction action, Set<Role> availableRoles) {
-                availableRoles.add(Role.EVEQUE);
-                availableRoles.add(Role.MAGICIEN);
-                availableRoles.add(Role.CONDOTTIERE);
-                availableRoles.add(Role.ROI);
-                action.pick(Role.ROI);
-                action.discard(Role.MAGICIEN);
-            }
-            @Override
-            public void playTurn(RegularTurnAction action, SelfPlayerView self, GameView gameState) throws IllegalActionException {
-            }
-        }));
-    }
-
+class DummyBehaviorTest {
     @Test
     void pickRoleTest() {
         DummyBehavior dummyBehavior = new DummyBehavior();
 
         // Set up available roles
-        Set<Role> availableRoles = new HashSet<>();
+        Set<Role> availableRoles = EnumSet.noneOf(Role.class);
         availableRoles.add(Role.ROI);
         availableRoles.add(Role.MARCHAND);
 
@@ -56,47 +31,4 @@ public class DummyBehaviorTest {
         assertTrue(availableRoles.contains(roleTurnAction.getDiscardedRole()));
         assertNotEquals(roleTurnAction.getPickedRole(), roleTurnAction.getDiscardedRole());
     }
-
-    /***
-    @Test
-    void playTurnTest() {
-
-
-        DummyBehavior dummyBehavior = new DummyBehavior();
-
-        // Set up a RegularTurnAction
-        RegularTurnAction regularTurnAction = new RegularTurnAction(new SelfPlayerView(new Player(new Behavior() {
-            @Override
-            public void pickRole(RoleTurnAction action, Set<Role> availableRoles) {
-                availableRoles.add(Role.EVEQUE);
-                availableRoles.add(Role.MAGICIEN);
-                availableRoles.add(Role.CONDOTTIERE);
-                availableRoles.add(Role.ROI);
-                action.pick(Role.ROI);
-                action.discard(Role.MAGICIEN);
-            }
-            @Override
-            public void playTurn(RegularTurnAction action, SelfPlayerView self, GameView gameState) {
-
-            }
-        })), null);
-
-        // Call playTurn method
-        assertDoesNotThrow(() -> dummyBehavior.playTurn(regularTurnAction, new SelfPlayerView(new Player(new Behavior() {
-            @Override
-            public void pickRole(RoleTurnAction action, Set<Role> availableRoles) throws IllegalActionException {
-
-            }
-
-            @Override
-            public void playTurn(RegularTurnAction action, SelfPlayerView self, GameView gameState) throws IllegalActionException {
-
-            }
-        })), null));
-
-        // Check if the basic action is valid
-        assertNotNull(regularTurnAction.getBasicAction());
-        assertTrue(regularTurnAction.getBasicAction() == RegularTurnAction.BasicAction.GOLD ||
-                regularTurnAction.getBasicAction() == RegularTurnAction.BasicAction.CARDS);
-    } ***/
 }
