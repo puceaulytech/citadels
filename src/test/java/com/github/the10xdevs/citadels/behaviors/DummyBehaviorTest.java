4e080adea0d8546320be2194e82dc9403fdd8095
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2023-12-06 13:25:19 +0100

    test: test dummybehavior with every possible set of roles (#2)

--- src/test/java/com/github/the10xdevs/citadels/behaviors/DummyBehaviorTest.java
+++ src/test/java/com/github/the10xdevs/citadels/behaviors/DummyBehaviorTest.java
@@ -4,22 +4,43 @@ import com.github.the10xdevs.citadels.interaction.actions.RoleTurnAction;
 import com.github.the10xdevs.citadels.interaction.behaviors.DummyBehavior;
 import com.github.the10xdevs.citadels.models.Role;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DummyBehaviorTest {
-    @Test
-    void pickRoleTest() {
-        DummyBehavior dummyBehavior = new DummyBehavior();
+    private static Stream<Set<Role>> generateRoles() {
+        Role[] roles = Role.values();
+        List<Set<Role>> allRoles = new ArrayList<>();
 
-        // Set up available roles
-        Set<Role> availableRoles = EnumSet.noneOf(Role.class);
-        availableRoles.add(Role.ROI);
-        availableRoles.add(Role.MARCHAND);
+        for (int i = 2; i <= Role.values().length; i++) {
+            generateCombinations(roles, allRoles, EnumSet.noneOf(Role.class), 0, i);
+        }
+
+        return allRoles.stream();
+    }
+
+    private static void generateCombinations(Role[] roles, List<Set<Role>> allCombinations, Set<Role> current, int start, int length) {
+        if (length == 0) {
+            allCombinations.add(EnumSet.copyOf(current));
+            return;
+        }
+
+        for (int i = start; i <= roles.length - length; i++) {
+            current.add(roles[i]);
+            generateCombinations(roles, allCombinations, current, i + 1, length - 1);
+            current.remove(roles[i]);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateRoles")
+    void pickRoleTest(Set<Role> availableRoles) {
+        DummyBehavior dummyBehavior = new DummyBehavior();
 
         // Create a RoleTurnAction
         RoleTurnAction roleTurnAction = new RoleTurnAction(Collections.unmodifiableSet(availableRoles));
