51e2d6ec336a4296c7fca735c62d4c403e5215e3
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2023-12-12 16:06:32 +0100

    refactor: rename dummy behavior (#13)

--- src/main/java/com/github/the10xdevs/citadels/Main.java
+++ src/main/java/com/github/the10xdevs/citadels/Main.java
@@ -1,14 +1,14 @@
 package com.github.the10xdevs.citadels;
 
 import com.github.the10xdevs.citadels.gamestate.Game;
-import com.github.the10xdevs.citadels.interaction.behaviors.DummyBehavior;
+import com.github.the10xdevs.citadels.interaction.behaviors.FastBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.RandomBehavior;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(List.of(new DummyBehavior(), new DummyBehavior(), new RandomBehavior(), new RandomBehavior()));
+        Game game = new Game(List.of(new FastBuilderBehavior(), new FastBuilderBehavior(), new RandomBehavior(), new RandomBehavior()));
         game.start();
     }
 }
