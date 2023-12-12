fb76cb03ed89e3c7d7cd6c542fc2806564ad2aa4
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2023-12-12 16:18:51 +0100

    feat: introduce expensive builder (#13)

--- src/main/java/com/github/the10xdevs/citadels/Main.java
+++ src/main/java/com/github/the10xdevs/citadels/Main.java
@@ -1,6 +1,7 @@
 package com.github.the10xdevs.citadels;
 
 import com.github.the10xdevs.citadels.gamestate.Game;
+import com.github.the10xdevs.citadels.interaction.behaviors.ExpensiveBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.FastBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.RandomBehavior;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(List.of(new FastBuilderBehavior(), new FastBuilderBehavior(), new RandomBehavior(), new RandomBehavior()));
+        Game game = new Game(List.of(new FastBuilderBehavior(), new ExpensiveBuilderBehavior(), new RandomBehavior(), new RandomBehavior()));
         game.start();
     }
 }
---
+++ src/main/java/com/github/the10xdevs/citadels/interaction/behaviors/ExpensiveBuilderBehavior.java
@@ -0,0 +1,81 @@
+package com.github.the10xdevs.citadels.interaction.behaviors;
+
+import com.github.the10xdevs.citadels.exceptions.IllegalActionException;
+import com.github.the10xdevs.citadels.interaction.actions.RegularTurnAction;
+import com.github.the10xdevs.citadels.interaction.actions.RoleTurnAction;
+import com.github.the10xdevs.citadels.interaction.views.GameView;
+import com.github.the10xdevs.citadels.interaction.views.SelfPlayerView;
+import com.github.the10xdevs.citadels.models.District;
+import com.github.the10xdevs.citadels.models.Role;
+import com.github.the10xdevs.citadels.utils.Pair;
+
+import java.util.*;
+
+/**
+ * A bot that tries to build expensive districts
+ */
+public class ExpensiveBuilderBehavior implements Behavior {
+    private static final List<Role> rolesImportance = List.of(
+            Role.ROI,
+            Role.CONDOTTIERE,
+            Role.VOLEUR,
+            Role.MARCHAND,
+            Role.EVEQUE,
+            Role.ASSASSIN,
+            Role.MAGICIEN,
+            Role.ARCHITECTE
+    );
+
+    private static Optional<Role> getMostImportantRole(Set<Role> availableRoles) {
+        return ExpensiveBuilderBehavior.rolesImportance.stream()
+                .filter(availableRoles::contains)
+                .findFirst();
+    }
+
+    @Override
+    public void pickRole(RoleTurnAction action, SelfPlayerView self, GameView gameState, Set<Role> availableRoles) throws IllegalActionException {
+        Set<Role> roles = EnumSet.copyOf(availableRoles);
+
+        Role roleToPick = ExpensiveBuilderBehavior.getMostImportantRole(roles).orElseThrow();
+        action.pick(roleToPick);
+        roles.remove(roleToPick);
+
+        Role roleToDiscard = ExpensiveBuilderBehavior.getMostImportantRole(roles).orElseThrow();
+        action.discard(roleToDiscard);
+    }
+
+    private static final int GOOD_DISTRICT_THRESHOLD = 4;
+    private District markedDistrict;
+
+    @Override
+    public void playTurn(RegularTurnAction action, SelfPlayerView self, GameView game) throws IllegalActionException {
+        if (markedDistrict != null) {
+            // Try to build our marked district
+            if (self.getGold() >= markedDistrict.getCost()) {
+                action.buildDistrict(markedDistrict);
+                markedDistrict = null;
+            } else {
+                action.takeGold();
+            }
+        } else {
+            Optional<District> bestDistrictInHand = self.getHand()
+                    .stream()
+                    .filter(district -> district.getCost() >= GOOD_DISTRICT_THRESHOLD)
+                    .filter(district -> !self.getCity().getDistricts().contains(district))
+                    .max(Comparator.comparingInt(District::getCost));
+
+            // Get the best good district in our hand
+            if (bestDistrictInHand.isPresent()) {
+                // Mark this district and try to build it the next turn
+                this.markedDistrict = bestDistrictInHand.get();
+                action.takeGold();
+            } else {
+                // Draw cards and choose the best district
+                Pair<District, District> cards = action.drawCards();
+
+                District bestDistrict = cards.first().getCost() > cards.second().getCost() ? cards.first() : cards.second();
+                action.chooseCard(bestDistrict);
+            }
+        }
+    }
+}
