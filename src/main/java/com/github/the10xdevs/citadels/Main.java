da5b4f57804642fdda0ab0f3263738383a9980bb
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2024-02-05 14:37:58 +0100

    feat: stats computation (#46)

--- src/main/java/com/github/the10xdevs/citadels/Main.java
+++ src/main/java/com/github/the10xdevs/citadels/Main.java
@@ -1,20 +1,24 @@
 package com.github.the10xdevs.citadels;
 
+import com.github.the10xdevs.citadels.bulk.BulkRunner;
 import com.github.the10xdevs.citadels.gamestate.Game;
 import com.github.the10xdevs.citadels.gamestate.GameBuilder;
 import com.github.the10xdevs.citadels.interaction.behaviors.ExpensiveBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.FastBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.RandomBehavior;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Game game = GameBuilder.create()
-                .addBehavior(new RandomBehavior())
-                .addBehavior(new RandomBehavior())
-                .addBehavior(new FastBuilderBehavior())
-                .addBehavior(new ExpensiveBuilderBehavior())
-                .build();
+        BulkRunner firstBulkRunner = new BulkRunner(1000, List.of(
+                new ExpensiveBuilderBehavior(),
+                new FastBuilderBehavior(),
+                new RandomBehavior(),
+                new RandomBehavior()
+        ));
 
-        game.start();
+        firstBulkRunner.run();
+        firstBulkRunner.printStats();
     }
 }
---
+++ src/main/java/com/github/the10xdevs/citadels/bulk/BulkRunner.java
@@ -0,0 +1,100 @@
+package com.github.the10xdevs.citadels.bulk;
+
+import com.github.the10xdevs.citadels.gamestate.Game;
+import com.github.the10xdevs.citadels.gamestate.GameBuilder;
+import com.github.the10xdevs.citadels.gamestate.Leaderboard;
+import com.github.the10xdevs.citadels.interaction.behaviors.Behavior;
+import com.github.the10xdevs.citadels.logging.VoidLogger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BulkRunner {
+    private final Map<Behavior, Result> scores = new HashMap<>();
+    private final int iterations;
+
+    public BulkRunner(int iterations,List<Behavior> behaviors) {
+        this.iterations = iterations;
+
+        for (Behavior b : behaviors) {
+            this.scores.put(b, new Result());
+        }
+    }
+
+    public void run() {
+        for (int i = 0; i < iterations; i++) {
+            GameBuilder gameBuilder = GameBuilder.create()
+                    .withLogger(new VoidLogger());
+
+            for (Behavior behavior : this.scores.keySet()) {
+                gameBuilder.addBehavior(behavior);
+            }
+
+            Game game = gameBuilder.build();
+
+            Leaderboard leaderboard = game.start();
+
+            for (int j = 0; j < leaderboard.getEntries().size(); j++) {
+                Leaderboard.Entry entry = leaderboard.getEntries().get(j);
+                Result bulkRunnerResult = this.scores.get(entry.getPlayer().getBehavior());
+
+                if (j == 0) {
+                    bulkRunnerResult.addWin();
+                } else {
+                    bulkRunnerResult.addLoss();
+                }
+
+                bulkRunnerResult.incrementAverageScore((double) entry.getScore() / iterations);
+            }
+        }
+    }
+
+    public void printStats() {
+        for (Map.Entry<Behavior, Result> entry : this.scores.entrySet()) {
+            Behavior behavior = entry.getKey();
+            Result result = entry.getValue();
+
+            double winPercentage = (result.getWins() * 100.0) / this.iterations;
+
+            System.out.format("%s - W/L %d/%d %.2f%% - avg %.1f\n", behavior.getName(), result.getWins(), result.getLoses(), winPercentage, result.getAverageScore());
+        }
+    }
+
+
+    public static class Result {
+        private int wins;
+        private int loses;
+        private double averageScore;
+
+        public Result() {
+            this.wins = 0;
+            this.loses = 0;
+            this.averageScore = 0;
+        }
+
+        public int getWins() {
+            return this.wins;
+        }
+
+        public int getLoses() {
+            return this.loses;
+        }
+
+        public double getAverageScore() {
+            return this.averageScore;
+        }
+
+        public void addWin() {
+            this.wins++;
+        }
+
+        public void addLoss() {
+            this.loses++;
+        }
+
+        public void incrementAverageScore(double amount) {
+            this.averageScore += amount;
+        }
+    }
+}
