02577c1b554abd864d3988a22929dbbf7eae0d5d
Logan Lucas <logan.lucas@etu.univ-cotedazur.fr>
2024-02-06 09:25:50 +0100

    feat: TryharderBehavior is decent (#36)

--- src/main/java/com/github/the10xdevs/citadels/Main.java
+++ src/main/java/com/github/the10xdevs/citadels/Main.java
@@ -4,12 +4,13 @@ import com.github.the10xdevs.citadels.gamestate.Game;
 import com.github.the10xdevs.citadels.interaction.behaviors.ExpensiveBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.FastBuilderBehavior;
 import com.github.the10xdevs.citadels.interaction.behaviors.RandomBehavior;
+import com.github.the10xdevs.citadels.interaction.behaviors.TryharderBehavior;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(List.of(new RandomBehavior(), new RandomBehavior(), new FastBuilderBehavior(), new ExpensiveBuilderBehavior()));
+        Game game = new Game(List.of(new RandomBehavior(), new TryharderBehavior(), new FastBuilderBehavior(), new ExpensiveBuilderBehavior()));
         game.start();
     }
 }
--- src/main/java/com/github/the10xdevs/citadels/interaction/behaviors/TryharderBehavior.java
+++ src/main/java/com/github/the10xdevs/citadels/interaction/behaviors/TryharderBehavior.java
@@ -3,10 +3,14 @@ package com.github.the10xdevs.citadels.interaction.behaviors;
 import com.github.the10xdevs.citadels.exceptions.IllegalActionException;
 import com.github.the10xdevs.citadels.interaction.actions.RegularTurnAction;
 import com.github.the10xdevs.citadels.interaction.actions.RoleTurnAction;
+import com.github.the10xdevs.citadels.interaction.actions.abilities.AssassinAbilityAction;
+import com.github.the10xdevs.citadels.interaction.actions.abilities.VoleurAbilityAction;
 import com.github.the10xdevs.citadels.interaction.views.GameView;
 import com.github.the10xdevs.citadels.interaction.views.SelfPlayerView;
 import com.github.the10xdevs.citadels.models.Category;
+import com.github.the10xdevs.citadels.models.District;
 import com.github.the10xdevs.citadels.models.Role;
+import com.github.the10xdevs.citadels.utils.Pair;
 
 import java.util.*;
 
@@ -21,14 +25,16 @@ public class TryharderBehavior implements Behavior {
             Role.MARCHAND,
             Role.ASSASSIN,
             Role.VOLEUR,
-            Role.ARCHITECTE,
-            Role.ROI,
-            Role.CONDOTTIERE,
             Role.EVEQUE,
+            Role.ARCHITECTE,
+            Role.CONDOTTIERE,
+            Role.ROI,
             Role.MAGICIEN
     );
 
-    private static final int TURN_THRESHOLD = 5;
+    private static final int TURN_THRESHOLD = 3;
+    private static final int GOLD_THRESHOLD = 10;
+    private static final int SCORE_THRESHOLD = 4;
 
     private static Optional<Role> getMostImportantRole(Set<Role> availableRoles, List<Role> wantedRoles) {
         return wantedRoles.stream()
@@ -42,6 +48,14 @@ public class TryharderBehavior implements Behavior {
                 .count();
     }
 
+    private static int compareCard(District d1, District d2, SelfPlayerView self, int turn) {
+        int scoreD1 = TryharderBehavior.getNumberOfBuiltDistrictByCategory(d1.getCategory(), self) == 0 ? 1 : 0;
+        int scoreD2 = TryharderBehavior.getNumberOfBuiltDistrictByCategory(d2.getCategory(), self) == 0 ? -1 : 0;
+        if (scoreD1 + scoreD2 == 0 || turn > TryharderBehavior.TURN_THRESHOLD * 2)
+            return d1.getScore() - d2.getScore();
+        return scoreD1 + scoreD2;
+    }
+
     @Override
     public void pickRole(RoleTurnAction action, SelfPlayerView self, GameView gameState, Set<Role> availableRoles) throws IllegalActionException {
         Set<Role> roles = EnumSet.copyOf(availableRoles);
@@ -53,15 +67,17 @@ public class TryharderBehavior implements Behavior {
                 action.pick(role.get());
                 roles.remove(role.get());
             }
-        } else {
-            // TODO
+        } else if (self.getGold() >= TryharderBehavior.GOLD_THRESHOLD && availableRoles.contains(Role.ARCHITECTE)) {
+            action.pick(Role.ARCHITECTE);
+            roles.remove(Role.ARCHITECTE);
         }
 
         if (action.getPickedRole() == null) {
             role = TryharderBehavior.allRoles.stream()
                     .filter(roles::contains)
                     .filter(r -> r.getCategory() != null)
-                    .min(Comparator.comparingInt(r -> TryharderBehavior.getNumberOfBuiltDistrictByCategory(r.getCategory(), self)));
+                    .filter(r -> TryharderBehavior.getNumberOfBuiltDistrictByCategory(r.getCategory(), self) != 0)
+                    .max(Comparator.comparingInt(r -> TryharderBehavior.getNumberOfBuiltDistrictByCategory(r.getCategory(), self)));
             if (role.isPresent()) {
                 action.pick(role.get());
                 roles.remove(role.get());
@@ -80,6 +96,68 @@ public class TryharderBehavior implements Behavior {
 
     @Override
     public void playTurn(RegularTurnAction action, SelfPlayerView self, GameView gameState) throws IllegalActionException {
+        int currentScoreThreshold = self.getCity().getDistricts().size() >= 4 ? TryharderBehavior.SCORE_THRESHOLD : TryharderBehavior.SCORE_THRESHOLD / 2;
 
+        if (self.getCurrentRole() == Role.MARCHAND || self.getGold() < TryharderBehavior.GOLD_THRESHOLD / 2) {
+            action.takeGold();
+        }
+
+        Optional<District> toBuild = self.getHand().stream()
+                .filter(district -> district.getScore() >= currentScoreThreshold)
+                .filter(district -> !self.getCity().getDistricts().contains(district))
+                .max((d1, d2) -> {
+                    if (TryharderBehavior.getNumberOfBuiltDistrictByCategory(d1.getCategory(), self) == 0)
+                        return 1;
+                    if (TryharderBehavior.getNumberOfBuiltDistrictByCategory(d2.getCategory(), self) == 0)
+                        return -1;
+                    return 0;
+                });
+
+        if (toBuild.isPresent() && toBuild.get().getCost() <= self.getGold()) {
+            action.buildDistrict(toBuild.get());
+        }
+
+        if (action.getBasicAction() != RegularTurnAction.BasicAction.GOLD && action.canDraw()) {
+            Pair<District, Optional<District>> cards = action.drawCards();
+
+            if (cards.second().isPresent())
+                action.chooseCard(
+                        TryharderBehavior.compareCard(cards.first(), cards.second().get(), self, gameState.getTurn()) > 0
+                                ? cards.first()
+                                : cards.second().get()
+                );
+            else
+                action.chooseCard(cards.first());
+        }
+
+        if (action.getBasicAction() == null)
+            action.takeGold();
+
+        switch (self.getCurrentRole()) {
+            case ASSASSIN: {
+                AssassinAbilityAction ability = (AssassinAbilityAction) action.getAbilityAction();
+                if (!gameState.getRolesFacingUp().contains(Role.MARCHAND))
+                    ability.kill(Role.MARCHAND);
+                else
+                    ability.kill(Role.MAGICIEN);
+                break;
+            }
+            case VOLEUR: {
+                VoleurAbilityAction ability = (VoleurAbilityAction) action.getAbilityAction();
+                Role roleToSteal = TryharderBehavior.allRoles.stream()
+                        .filter(role -> role != Role.ASSASSIN)
+                        .filter(role -> gameState.getKilledRole().isEmpty() || role != gameState.getKilledRole().get())
+                        .filter(role -> role != self.getCurrentRole())
+                        .filter(role -> !gameState.getRolesFacingUp().contains(role))
+                        .findFirst().orElseThrow();
+                ability.stealFrom(roleToSteal);
+                break;
+            }
+            case ARCHITECTE: {
+                // TODO
+                break;
+            }
+            default:
+        }
     }
 }
