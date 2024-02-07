dee4a753e3e4ebf2203406b0af0692d9b99fc588
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2024-02-07 10:29:51 +0100

    fix: ConsoleLogger was crappy, this is deliverance (#51)

--- src/main/java/com/github/the10xdevs/citadels/logging/ConsoleLogger.java
+++ src/main/java/com/github/the10xdevs/citadels/logging/ConsoleLogger.java
@@ -10,7 +10,6 @@ import com.github.the10xdevs.citadels.models.District;
 import com.github.the10xdevs.citadels.models.Role;
 
 import java.io.BufferedOutputStream;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Optional;
 
@@ -28,7 +27,7 @@ public class ConsoleLogger implements Logger {
     public static final String ANSI_SILVER = "\u001B[38;2;180;180;180m";
     public static final String ANSI_BRONZE = "\u001B[38;2;106;56;5m";
 
-    private final BufferedOutputStream outputStream = new BufferedOutputStream(System.out);
+    private final PrintWriter writer = new PrintWriter(new BufferedOutputStream(System.out));
     private final boolean supportsColor;
 
     public ConsoleLogger() {
@@ -37,138 +36,138 @@ public class ConsoleLogger implements Logger {
 
     @Override
     public void logTurnStart(int turn) {
-        this.println();
-        this.print("------ Tour n°");
-        this.printInt(turn);
-        this.println(" ------");
+        writer.println();
+        writer.print("------ Tour n°");
+        writer.print(turn);
+        writer.println(" ------");
 
-        this.flush();
+        writer.flush();
     }
 
     @Override
     public void logRoleTurnAction(int index, Player player, RoleTurnAction action) {
-        this.println();
-        this.print("--- Joueur n°");
-        this.printInt(index + 1);
-        this.print(" (");
-        this.print(player.getBehavior().getName());
-        this.println(") ---");
-        this.print("Choisit le rôle ");
+        writer.println();
+        writer.print("--- Joueur n°");
+        writer.print(index + 1);
+        writer.print(" (");
+        writer.print(player.getBehavior().getName());
+        writer.println(") ---");
+        writer.print("Choisit le rôle ");
         this.printColorized(action.getPickedRole());
-        this.println();
+        writer.println();
         if (action.getDiscardedRole() != null) {
-            this.print("Défausse le rôle ");
+            writer.print("Défausse le rôle ");
             this.printColorized(action.getDiscardedRole());
-            this.println();
+            writer.println();
         }
 
-        this.flush();
+        writer.flush();
     }
 
     private void logAbilityAction(Player player, AbilityAction action) {
         if (player.getCurrentRole() == Role.ASSASSIN) {
             AssassinAbilityAction assassinAction = (AssassinAbilityAction) action;
             if (assassinAction.getKilledRole() != null) {
-                this.print("Assassine le rôle ");
+                writer.print("Assassine le rôle ");
                 this.printColorized(assassinAction.getKilledRole());
-                this.println();
+                writer.println();
             }
         } else if (player.getCurrentRole() == Role.VOLEUR) {
             VoleurAbilityAction voleurAction = (VoleurAbilityAction) action;
             if (voleurAction.getStolenRole() != null) {
-                this.print("Vole le rôle ");
+                writer.print("Vole le rôle ");
                 this.printColorized(voleurAction.getStolenRole());
-                this.println();
+                writer.println();
             }
         } else if (player.getCurrentRole() == Role.MAGICIEN) {
             MagicienAbilityAction magicienAction = (MagicienAbilityAction) action;
             if (magicienAction.getExchangedPlayer() != null) {
-                this.print("Echange ses cartes avec ");
-                this.println(magicienAction.getExchangedPlayer().getName());
+                writer.print("Echange ses cartes avec ");
+                writer.println(magicienAction.getExchangedPlayer().getName());
             }
         } else if (player.getCurrentRole() == Role.CONDOTTIERE) {
             CondottiereAbilityAction condottiereAction = (CondottiereAbilityAction) action;
             if (condottiereAction.getTargetDistrict() != null) {
-                this.print("Détruit ");
+                writer.print("Détruit ");
                 this.printColorized(condottiereAction.getTargetDistrict());
-                this.print(" chez ");
-                this.println(condottiereAction.getTargetPlayer().getName());
+                writer.print(" chez ");
+                writer.println(condottiereAction.getTargetPlayer().getName());
             }
         }
     }
 
     @Override
     public void logRegularTurnAction(Player player, RegularTurnAction action) {
-        this.println();
-        this.print("--- Joueur ayant le rôle ");
+        writer.println();
+        writer.print("--- Joueur ayant le rôle ");
         this.printColorized(player.getCurrentRole());
-        this.print(" (");
-        this.print(player.getBehavior().getName());
-        this.println(") ---");
+        writer.print(" (");
+        writer.print(player.getBehavior().getName());
+        writer.println(") ---");
 
         this.logAbilityAction(player, action.getAbilityAction());
 
         if (action.getBasicAction() == RegularTurnAction.BasicAction.GOLD) {
-            this.println("Prend deux pièces d'or");
+            writer.println("Prend deux pièces d'or");
         } else if (action.getBasicAction() == RegularTurnAction.BasicAction.CARDS) {
             Optional<District> discarded = action.getDiscardedCard();
             if (discarded.isPresent()) {
-                this.println("Pioche deux cartes");
-                this.print("    garde    ");
+                writer.println("Pioche deux cartes");
+                writer.print("    garde    ");
                 this.printColorized(action.getChosenCard());
-                this.println();
-                this.print("    défausse ");
+                writer.println();
+                writer.print("    défausse ");
                 this.printColorized(discarded.get());
-                this.println();
+                writer.println();
             } else {
-                this.println("Pioche une carte");
-                this.print("    garde    ");
+                writer.println("Pioche une carte");
+                writer.print("    garde    ");
                 this.printColorized(action.getChosenCard());
-                this.println();
+                writer.println();
             }
         }
 
         if (action.getBuiltDistrict() != null) {
-            this.print("Construit le quartier ");
+            writer.print("Construit le quartier ");
             this.printColorized(action.getBuiltDistrict());
-            this.println();
+            writer.println();
         }
 
-        this.flush();
+        writer.flush();
     }
 
     @Override
     public void logWinners(Leaderboard leaderboard) {
-        this.println("\n------ Podium ------");
+        writer.println("\n------ Podium ------");
         int rank = 1;
         for (Leaderboard.Entry entry : leaderboard.getEntries()) {
             if (this.supportsColor) {
                 if (rank == 1) {
-                    this.print(ANSI_GOLD);
+                    writer.print(ANSI_GOLD);
                 } else if (rank == 2) {
-                    this.print(ANSI_SILVER);
+                    writer.print(ANSI_SILVER);
                 } else if (rank == 3) {
-                    this.print(ANSI_BRONZE);
+                    writer.print(ANSI_BRONZE);
                 }
             }
 
-            this.print("-> ");
-            this.print(entry.getPlayer().getBehavior().getName());
-            this.print(" avec ");
-            this.printInt(entry.getScore());
-            this.println(" points");
+            writer.print("-> ");
+            writer.print(entry.getPlayer().getBehavior().getName());
+            writer.print(" avec ");
+            writer.print(entry.getScore());
+            writer.println(" points");
 
             if (rank <= 3 && this.supportsColor) {
-                this.print(ANSI_RESET);
+                writer.print(ANSI_RESET);
             }
 
             for (District district : entry.getPlayer().getCity()) {
-                this.print("  - ");
+                writer.print("  - ");
                 this.printColorized(district);
-                this.println();
+                writer.println();
             }
-            this.println();
-            this.flush();
+            writer.println();
+            writer.flush();
 
             rank++;
         }
@@ -176,88 +175,42 @@ public class ConsoleLogger implements Logger {
 
     @Override
     public void logError(Throwable error) {
-        this.println();
-        this.println("------ " + ANSI_RED + "Error" + ANSI_RESET + " ------");
-        this.println("Something went wrong during play: ");
-        error.printStackTrace(new PrintWriter(this.outputStream, true));
-        this.flush();
-    }
-
-    private void println() {
-        try {
-            outputStream.write(System.lineSeparator().getBytes());
-        } catch (IOException e) {
-            this.unexpectedExit(e);
-        }
-    }
-
-    private void println(String text) {
-        try {
-            outputStream.write(text.getBytes());
-            outputStream.write(System.lineSeparator().getBytes());
-        } catch (IOException e) {
-            this.unexpectedExit(e);
-        }
-    }
-
-    private void printInt(int value) {
-        try {
-            outputStream.write(String.valueOf(value).getBytes());
-        } catch (IOException e) {
-            this.unexpectedExit(e);
-        }
-    }
-
-    private void print(String text) {
-        try {
-            outputStream.write(text.getBytes());
-        } catch (IOException e) {
-            this.unexpectedExit(e);
-        }
-    }
-
-    private void flush() {
-        try {
-            outputStream.flush();
-        } catch (IOException e) {
-            this.unexpectedExit(e);
-        }
+        writer.println();
+        writer.println("------ " + ANSI_RED + "Error" + ANSI_RESET + " ------");
+        writer.println("Something went wrong during play: ");
+        error.printStackTrace(this.writer);
+        writer.flush();
     }
 
     private void printColorized(Role role) {
         Category category = role.getCategory();
         if (category != null && this.supportsColor)
-            this.print(role.getCategory().getANSIColorCode());
+            writer.print(role.getCategory().getANSIColorCode());
 
-        this.print(role.toString());
+        writer.print(role);
 
         if (category != null && this.supportsColor)
-            this.print(ANSI_RESET);
+            writer.print(ANSI_RESET);
     }
 
     private void printColorized(District district) {
-        this.print(district.getName());
-        this.print(", ");
+        writer.print(district.getName());
+        writer.print(", ");
 
         if (this.supportsColor)
-            this.print(district.getCategory().getANSIColorCode());
+            writer.print(district.getCategory().getANSIColorCode());
 
-        this.print(district.getCategory().toString());
+        writer.print(district.getCategory().toString());
 
         if (this.supportsColor)
-            this.print(ANSI_RESET);
+            writer.print(ANSI_RESET);
 
-        this.print(", prix: ");
-        this.printInt(district.getCost());
+        writer.print(", prix: ");
+        writer.print(district.getCost());
 
         if (district.getCost() != district.getScore()) {
-            this.print(", score: ");
-            this.printInt(district.getScore());
+            writer.print(", score: ");
+            writer.print(district.getScore());
         }
-    }
-
-    private void unexpectedExit(Throwable e) {
-        System.out.println("Error on stdout: " + e);
-        System.exit(1);
     }
 }
