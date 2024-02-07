d35b0b468b037851fb0228a048636901b3e4a35f
Saad Benaqa <saad.benaqa@etu.unice.fr>
2024-02-07 11:26:59 +0100

    tests : add some tests

--- src/test/java/com/github/the10xdevs/citadels/utils/RandomUtilsTest.java
+++ src/test/java/com/github/the10xdevs/citadels/utils/RandomUtilsTest.java
@@ -2,6 +2,7 @@ package com.github.the10xdevs.citadels.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -22,6 +23,21 @@ class RandomUtilsTest {
         int element = RandomUtils.chooseFrom(new Random(), integers);
 
         assertTrue(integers.contains(element));
+    }
+    @Test
+    public void testChooseFromThrowsException() {
+        Random faultyRandomGenerator = new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return bound; // This will be out of bounds for any list
+            }
+        };
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        assertThrows(IllegalStateException.class, () -> {
+            RandomUtils.chooseFrom(faultyRandomGenerator, list);
+        });
     }
 
 
