c1d8e974045c7aa51bdc088888b3f2abf462cd5a
Logan Lucas <logan.lucas@etu.univ-cotedazur.fr>
2024-02-07 13:45:25 +0100

    test: get rid of wtf tests

--- src/test/java/com/github/the10xdevs/citadels/interaction/actions/RoleTurnActionTest.java
+++ src/test/java/com/github/the10xdevs/citadels/interaction/actions/RoleTurnActionTest.java
@@ -1,23 +1,12 @@
 package com.github.the10xdevs.citadels.interaction.actions;
 
-import com.github.the10xdevs.citadels.exceptions.DuplicatedDistrictException;
 import com.github.the10xdevs.citadels.exceptions.IllegalActionException;
-import com.github.the10xdevs.citadels.gamestate.Deck;
-import com.github.the10xdevs.citadels.gamestate.Game;
-import com.github.the10xdevs.citadels.gamestate.Player;
 import com.github.the10xdevs.citadels.interaction.behaviors.Behavior;
-import com.github.the10xdevs.citadels.interaction.behaviors.RandomBehavior;
-import com.github.the10xdevs.citadels.interaction.behaviors.TryharderBehavior;
 import com.github.the10xdevs.citadels.interaction.views.GameView;
 import com.github.the10xdevs.citadels.interaction.views.SelfPlayerView;
-import com.github.the10xdevs.citadels.logging.VoidLogger;
-import com.github.the10xdevs.citadels.models.Category;
-import com.github.the10xdevs.citadels.models.District;
 import com.github.the10xdevs.citadels.models.Role;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -106,50 +95,4 @@ class RoleTurnActionTest {
 
         assertThrows(IllegalActionException.class, () -> testBehavior.pickRole(action, null, null));
     }
-
-
-    @Test
-    void gameEndsWhenPlayerHasEightDistricts() throws DuplicatedDistrictException {
-        List<Behavior> behaviors = new ArrayList<>();
-        behaviors.add(new TryharderBehavior());
-        behaviors.add(new RandomBehavior());
-        Deck<District> deck = new Deck<>(District.all());
-        Game game = new Game(behaviors, deck, new VoidLogger());
-        game.start();
-        Player player = game.getPlayers().get(0);
-        for (int i = 0; i < 8; i++) {
-            player.getCity().addDistrict(new District("District " + i, Category.RELIGIEUX, i));
-        }
-        assertTrue(game.isGameOver());
-    }
-
-    @Test
-    void gameContinuesWhenPlayerHasLessThanEightDistricts() throws DuplicatedDistrictException {
-        List<Behavior> behaviors = new ArrayList<>();
-        behaviors.add(new TryharderBehavior());
-        behaviors.add(new RandomBehavior());
-        Deck<District> deck = new Deck<>(District.all());
-        Game game = new Game(behaviors, deck, new VoidLogger());
-        game.start();
-        Player player = game.getPlayers().get(0);
-        for (int i = 0; i < 7; i++) {
-            player.getCity().addDistrict(new District("District " + i, Category.RELIGIEUX, i));
-        }
-        assertTrue(game.isGameOver());
-    }
-
-    @Test
-    void playerWithRoleKingBecomesFirstPlayerNextTurn() {
-        List<Behavior> behaviors = new ArrayList<>();
-        behaviors.add(new TryharderBehavior());
-        behaviors.add(new RandomBehavior());
-        Deck<District> deck = new Deck<>(District.all());
-        Game game = new Game(behaviors, deck, new VoidLogger());
-        game.start();
-        Player player = game.getPlayers().get(0);
-        player.setCurrentRole(Role.ROI);
-        assertEquals(player, game.getPlayers().get(0));
-    }
-
-
 }
