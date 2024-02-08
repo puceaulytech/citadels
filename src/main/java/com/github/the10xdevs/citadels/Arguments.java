002fde90cbc82b0a45c35e7f581baceefd31bb73
Romain Chardiny <romain.chardiny@etu.univ-cotedazur.fr>
2024-02-08 09:55:32 +0100

    fix: 2thousands is the same as 2thousand

--- src/main/java/com/github/the10xdevs/citadels/Arguments.java
+++ src/main/java/com/github/the10xdevs/citadels/Arguments.java
@@ -3,7 +3,7 @@ package com.github.the10xdevs.citadels;
 import com.beust.jcommander.Parameter;
 
 public class Arguments {
-    @Parameter(names = "--2thousand")
+    @Parameter(names = {"--2thousand", "--2thousands"})
     public boolean twoThousand;
 
     @Parameter(names = "--demo")
